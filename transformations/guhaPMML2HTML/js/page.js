f8e7c9388e758ce849e3d94ed5a423c937ddc20b
Stanislav Vojíř <stanislav.vojir@gmail.com>
2015-06-28 00:59:23 +0200

    vyřešení problému se skrolováním při přesunu menu

--- transformations/guhaPMML2HTML/js/page.js
+++ transformations/guhaPMML2HTML/js/page.js
@@ -5,16 +5,29 @@
 $(document).ready(function(){
   var navigationElement=$('section#navigation');
   var navigationElementTop=navigationElement.offset().top-20;
-  if ($(document).scrollTop()>navigationElementTop){
-    navigationElement.addClass('onTop');
-  }
 
-  $(window).scroll(function(){
+  $('section#navigation a[href^="#"]').click(function(event){
+    var elementId=$(this).attr('href');
+    var element=$(elementId);
+    if (element){
+      event.preventDefault();
+      navigationElement.addClass('onTop');
+      $(window).scrollTop(element.position().top);
+    }
+  });
+
+  var solveNavigationPosition = function(){
     if ($(document).scrollTop()>navigationElementTop){
       navigationElement.addClass('onTop');
     }else{
       navigationElement.removeClass('onTop');
     }
+  };
+
+  solveNavigationPosition();
+
+  $(window).scroll(function(){
+    solveNavigationPosition();
   });
 });
 
